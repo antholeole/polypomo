d8adad0e31b8f2e82adcae068ac3cf0cdd7ba734
Anthony Oleinik <antholeinik@gmail.com>
2023-04-11 13:29:19 -0500

    updated to standard flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,20 @@
 {
   description = "a polybar pomodoro widget";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
   outputs = { self, nixpkgs }: {
-    overlay = (self: super: {
-        polypomo = nixpkgs.stdenv.mkDerivation {
+    overlay = final: prev: {
+        polypomo = final.mkDerivation {
             name = "polypomo";
             propagatedBuildInputs = [
-              nixpkgs.python3_11
+              final.python3_11
             ];
             dontUnpack = true;
             installPhase = "install -Dm755 ${./polypomo.py} $out/bin/polypomo";
         };
-    });
+  };
   };
 }
