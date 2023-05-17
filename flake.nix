4cbbe83824300bc652728e1e32caa5c024a0dfd3
Anthony Oleinik <antholeinik@gmail.com>
2023-05-17 18:25:30 -0500

    this is what I call

--- flake.nix
+++ flake.nix
@@ -1,31 +1,36 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, fenix, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
+  outputs = { self, flake-utils, naersk, nixpkgs }:
+    let
+      rustPkg = flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = (import nixpkgs) {
+            inherit system;
+          };
 
-        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-      in
+          naersk' = pkgs.callPackage naersk { };
 
-      {
-        defaultPackage = (pkgs.makeRustPlatform {
-          inherit (fenix.packages.${system}.minimal) cargo rustc;
-        }).buildRustPackage {
-          pname = manifest.name;
-          version = manifest.version;
-          cargoLock.lockFile = ./Cargo.lock;
-          src = pkgs.lib.cleanSource ./.;
-        };
-      });
+        in {
+          defaultPackage = naersk'.buildPackage {
+            src = ./.;
+          };
+
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo ];
+          };
+        }
+      );
+    in
+    {
+      defaultPackage = rustPkg.defaultPackage;
+      devShell = rustPkg.devShell;
+      overlays.default = prev: final: {
+        polydoro = rustPkg.defaultPackage."${final.system}";
+      };
+    };
 }
