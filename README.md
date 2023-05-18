5d02ed8d762b4c51770950bca52a40cb788a4b9d
Anthony Oleinik <antholeinik@gmail.com>
2023-05-18 18:00:25 -0500

    documentation

---
+++ README.md
@@ -0,0 +1,14 @@
+A customizable polybar pomodoro timer. 
+
+### What is a pomodoro timer?
+
+For focused work sessions, 25 minute "deep work" with 5 minute rest periods interweaven allow for maximized deep work. Every 4 deep work sessions, reward yourself with a longer 25 minute deep work session.
+
+### Features
+
+- Fully customizable icons
+- Fully customizable cycles (durations, number of cycles)
+- Notify-Send for notifications. 
+- Utilizes unix fs pipes - run many pomodoro timers with many clients (if necessary).
+- Simple, elegant defaults.
+- Fully nix compatible: exports an overlay as well as default packaging. 
--- src/cli.rs
+++ src/cli.rs
@@ -12,15 +12,24 @@ const POLYDORO_SOCKET_NAME: &str = "polydoro";
 
 #[derive(Subcommand)]
 pub enum Commands {
-    /// Run polydoro.
+    /// Runs polydoro.
+    /// 
+    /// This starts a polydoro server on a local socket, specified by puid. 
     Run(RunArgs),
 
+    /// Pauses / unpauses another polydoro timer that is running.
     Toggle {
+        /// Specifies which polydoro timer to toggle. 
+        /// 
+        /// This default corresponds to the default in "run".
+        /// If running on the non-default port, make sure to specify this.
         #[arg(short, long, default_value = POLYDORO_SOCKET_NAME)]
         puid: String,
     },
 
     Skip {
+        /// Entirely skips the current polydoro state. If resting, will go to a work state
+        /// and visa-versa. Also increments the counter as normal.
         #[arg(short, long, default_value = POLYDORO_SOCKET_NAME)]
         puid: String,
     }
@@ -29,32 +38,59 @@ pub enum Commands {
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 pub struct RunArgs {
+    /// The socket that the polydoro timer runs on. 
+    /// 
+    /// By default, runs in a socket caled "polydoro". This socket will appear as a file
+    /// in the /tmp/ directory (ex. if left default, /tmp/polydoro). 
     #[arg(short, long, default_value = POLYDORO_SOCKET_NAME)]
     pub puid: String,
 
+    /// the icon that is displayed when currently resting. 
+    /// 
+    /// For use with wide unicode characters, prepend and append spaces,
+    /// i.e. " 󰒲 ".
     #[arg(long, default_value = " 󰒲 ")]
     pub sleeping_icon: String,
 
+    /// the icon that is displayed when currently working. 
+    /// 
+    /// For use with wide unicode characters, prepend and append spaces,
+    /// i.e. " 󰱠 ".
     #[arg(long, default_value = " 󰱠 ")]
     pub working_icon: String,
 
+    /// the icon that is displayed when currently paused. 
+    /// 
+    /// For use with wide unicode characters, prepend and append spaces,
+    /// i.e. "  ".
     #[arg(long, default_value = "  ")]
     pub paused_icon: String,
 
+    /// The length of time spent resting during a regular rest. 
+    /// 
+    /// This value is set in seconds. Default is 5 minutes (5 * 60).
     #[arg(long, default_value_t = 60 * 5)]
     pub rest_period_s: u16,
 
+    /// The length of time spent during a work period. Set in seconds.
+    /// 
+    /// By default, this is 25 minutes. 
     #[arg(long, default_value_t = 60 * 25)]
     pub work_period_s: u16,
 
+    /// The length of time spent during a long break.
     #[arg(long, default_value_t = 60 * 30)]
     pub break_period_s: u16,
 
     /// how many cycles before a long break. Defaults to four 
-    /// (so breaks are: 5, 5, 5, 5, 25)
+    /// (ex. breaks are: 5, 5, 5, 5, 30, each with a 25 minute work period between.)
     #[arg(long, default_value_t = 4)]
     pub cycles: i8,
 
-    #[arg(long, default_value_t = 750)]
+    /// How quickly the server writes the time to the polybar. Unit is ms.
+    /// 
+    ///  Defaults to 500ms. To minimize "time skip" effects, set a number that multiples well into
+    /// 1. (500, 250, 100, 50, etc.). This way, there won't be longer periods of non-updating.
+    #[arg(long, default_value_t = 500)]
     pub refresh_rate_ms: u64, 
 }
